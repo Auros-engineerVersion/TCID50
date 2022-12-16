be7247418ba2f8ff3d96fc93aafd4726c4d11ad1
Auros-NotePC <92504306+Auros-engineerVersion@users.noreply.github.com>
2022-12-16 10:38:02 +0900

    Conmpete theKarberMethod

--- TCID50.ps1
+++ TCID50.ps1
@@ -5,35 +5,48 @@ function TCID50 {
     [CmdletBinding()]
     param (
         [string]$Path,
-        [Int32]$Magn
+        [Int32]$Magn,
+        [string]$Method,
+        [bool]$IsLog = $false
     )
 
     $values = Import-Csv -Path $Path -Delimiter "," -Encoding Default
     [string[]]$headers = $values | Get-Member -MemberType 'NoteProperty' | Select-Object -ExpandProperty 'Name'
 
-    [double]$sigma = -0.5
-    foreach ($header in $headers) {
-        #50%以上顕性の数を取得
-        $halfOvertNum = 0
-        foreach ($v in $values.$header) {
-            $halfOvertNum += $v
+    [double]$result = 0
+    switch ($Method) {
+        Default {
+            $result = ImprovedKarberMethod -Values $values -DilRates $headers -Magn $Magn
         }
-        $sigma += $halfOvertNum / $values.Count
     }
 
-    Write-Host "sigam is $sigma"
-
-    #一列目の希釈倍率
-    [double]$firstLineRate = [Math]::Pow($Magn, $headers[0])
-
-    <#
-    Karberの式より
-    TCID50＝（1列目の希釈率）×{(希釈倍率)^(Σ－0.5)}
-    この場合、Σには最初から-0.5を引いてある。
-    #>
-    [double]$result = $firstLineRate * [Math]::Pow($Magn, $sigma)
+    $result = $IsLog ? [Math]::Log($result, $Magn) : $result
     return ($result)
 }
 
-$samplePath = "$PSScriptRoot\sample_01.csv"
-TCID50 -Path $samplePath -Magn 10
+function ImprovedKarberMethod {
+    [CmdletBinding()]
+    param (
+        [Object[]]$Values,
+        [string[]]$DilRates,
+        [Int32]$Magn
+    )
+
+    [double]$sigma = 0
+    foreach ($rate in $DilRates) {
+        #50%以上顕性の数を取得
+        $halfOvertNum = 0
+        foreach ($v in $Values.$rate) {
+            $halfOvertNum += $v
+        }
+
+        $sigma += $halfOvertNum / $values.Count
+    }
+
+    #TCID50 ＝（最も低い稀釈倍率）×{(段階稀釈した倍率)^(Σ－0.5)}
+    [double]$result = [Math]::Pow($Magn, $DilRates[0]) * [Math]::Pow($Magn, $sigma - 0.5)
+    return ($result)
+}
+
+$samplePath = "$PSScriptRoot\sample_03.csv"
+TCID50 -Path $samplePath -Magn 10 -IsLog $true
